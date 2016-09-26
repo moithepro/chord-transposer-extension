3e2fcb93dd93ca55322db2a6a71807dcfd7605d9
Domenico Gemoli <domenicogemoli@gmail.com>
2016-09-26 21:32:32 +0200

    Fix lint

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
-var fstream = require('fstream'),
-    tar = require('tar'),
-    zlib = require('zlib');
+var fstream = require('fstream')
+var tar = require('tar')
+var zlib = require('zlib')
 
-fstream.Reader('release/') /* Read the source directory */
-.pipe(tar.Pack()) /* Convert the directory to a .tar file */
-.pipe(zlib.Gzip()) /* Compress the .tar file */
-.pipe(fstream.Writer({ 'path': 'release.tar.gz' })); /* Give the output file name */
+fstream.Reader('release/')
+  .pipe(tar.Pack())
+  .pipe(zlib.Gzip())
+  .pipe(fstream.Writer({
+    'path': 'release.tar.gz'
+  }))
