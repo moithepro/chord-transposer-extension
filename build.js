f03852814bfc07b3bbe9bdd950d13e8229b2b0f5
Domenico Gemoli <domenicogemoli@gmail.com>
2016-09-26 21:18:31 +0200

    Just create zip

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
-require('dotenv').config({silent: true})
-require('./build/app').load()
+var fstream = require('fstream'),
+    tar = require('tar'),
+    zlib = require('zlib');
 
-var createCrx = require('./build/createCrx.js')
-var uploadViaFTP = require('./build/uploadViaFTP')
-
-createCrx()
-  .then(uploadViaFTP)
+fstream.Reader('release/') /* Read the source directory */
+.pipe(tar.Pack()) /* Convert the directory to a .tar file */
+.pipe(zlib.Gzip()) /* Compress the .tar file */
+.pipe(fstream.Writer({ 'path': 'release.tar.gz' })); /* Give the output file name */
