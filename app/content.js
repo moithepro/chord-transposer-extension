4ac3d5d0df4ee3e41b35914befb14547e695f20a
moithepro <52801196+moithepro@users.noreply.github.com>
2023-10-17 21:08:29 +0300

    update logic

--- app/content.js
+++ app/content.js
@@ -1,80 +1,167 @@
-function transposeChord( chord, amount ) {
-    const sharpnotes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
-    const flatnotes  = ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"];
-    let rootChord = chord[0];
-    if(chord[1] === '#' || chord[1] == 'b') {
-        rootChord += chord[1];
-    }
-    amount = (amount % sharpnotes.length) || 1;
-    if(amount < 0) { amount += sharpnotes.length; }
-    for(let note=0; note < sharpnotes.length; ++note) {
-        if(rootChord === sharpnotes[note]) {
-            return( (sharpnotes[(note + amount) % sharpnotes.length]) + chord.substr(rootChord.length) );
-        }
-        if(rootChord === flatnotes[note]) {
-            return( (flatnotes[(note + amount) % flatnotes.length]) + chord.substr(rootChord.length) );
-        }
-    }
-    return ('???');
+// not used yet
+// const MajorChordsSharp = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
+// const MajorChordsFlat  = ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"];
+// const MinorChordsSharp = ["Am","A#m","Bm","Cm","C#m","Dm","D#m","Em","Fm","F#m","Gm","G#m"];
+// const MinorChordsFlat  = ["Am","Bbm","Bm","Cm","Dbm","Dm","Ebm","Em","Fm","Gbm","Gm","Abm"];
+// const MajorChordsSharp7 = ["A7","A#7","B7","C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7"];
+// const MajorChordsFlat7  = ["A7","Bb7","B7","C7","Db7","D7","Eb7","E7","F7","Gb7","G7","Ab7"];
+// const MinorChordsSharp7 = ["Am7","A#m7","Bm7","Cm7","C#m7","Dm7","D#m7","Em7","Fm7","F#m7","Gm7","G#m7"];
+// const MinorChordsFlat7  = ["Am7","Bbm7","Bm7","Cm7","Dbm7","Dm7","Ebm7","Em7","Fm7","Gbm7","Gm7","Abm7"];
+// const MajorChordsSharp9 = ["A9","A#9","B9","C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9"];
+// const MajorChordsFlat9  = ["A9","Bb9","B9","C9","Db9","D9","Eb9","E9","F9","Gb9","G9","Ab9"];
+// const MinorChordsSharp9 = ["Am9","A#m9","Bm9","Cm9","C#m9","Dm9","D#m9","Em9","Fm9","F#m9","Gm9","G#m9"];
+// const MinorChordsFlat9  = ["Am9","Bbm9","Bm9","Cm9","Dbm9","Dm9","Ebm9","Em9","Fm9","Gbm9","Gm9","Abm9"];
+// const MajorChordsSharp11 = ["A11","A#11","B11","C11","C#11","D11","D#11","E11","F11","F#11","G11","G#11"];
+// const MajorChordsFlat11  = ["A11","Bb11","B11","C11","Db11","D11","Eb11","E11","F11","Gb11","G11","Ab11"];
+// const MinorChordsSharp11 = ["Am11","A#m11","Bm11","Cm11","C#m11","Dm11","D#m11","Em11","Fm11","F#m11","Gm11","G#m11"];
+// const MinorChordsFlat11  = ["Am11","Bbm11","Bm11","Cm11","Dbm11","Dm11","Ebm11","Em11","Fm11","Gbm11","Gm11","Abm11"];
+// const MajorChordsSharp13 = ["A13","A#13","B13","C13","C#13","D13","D#13","E13","F13","F#13","G13","G#13"];
+// const MajorChordsFlat13  = ["A13","Bb13","B13","C13","Db13","D13","Eb13","E13","F13","Gb13","G13","Ab13"];
+// const MinorChordsSharp13 = ["Am13","A#m13","Bm13","Cm13","C#m13","Dm13","D#m13","Em13","Fm13","F#m13","Gm13","G#m13"];
+// const MinorChordsFlat13  = ["Am13","Bbm13","Bm13","Cm13","Dbm13","Dm13","Ebm13","Em13","Fm13","Gbm13","Gm13","Abm13"];
+// const MajorChordsSharpMaj7 = ["Amaj7","A#maj7","Bmaj7","Cmaj7","C#maj7","Dmaj7","D#maj7","Emaj7","Fmaj7","F#maj7","Gmaj7","G#maj7"];
+// const MajorChordsFlatMaj7  = ["Amaj7","Bbmaj7","Bmaj7","Cmaj7","Dbmaj7","Dmaj7","Ebmaj7","Emaj7","Fmaj7","Gbmaj7","Gmaj7","Abmaj7"];
+// const MinorChordsSharpMaj7 = ["Amaj7","A#maj7","Bmaj7","Cmaj7","C#maj7","Dmaj7","D#maj7","Emaj7","Fmaj7","F#maj7","Gmaj7","G#maj7"];
+// const MinorChordsFlatMaj7  = ["Amaj7","Bbmaj7","Bmaj7","Cmaj7","Dbmaj7","Dmaj7","Ebmaj7","Emaj7","Fmaj7","Gbmaj7","Gmaj7","Abmaj7"];
+// const MajorChordsSharpMaj9 = ["Amaj9","A#maj9","Bmaj9","Cmaj9","C#maj9","Dmaj9","D#maj9","Emaj9","Fmaj9","F#maj9","Gmaj9","G#maj9"];
+// const MajorChordsFlatMaj9  = ["Amaj9","Bbmaj9","Bmaj9","Cmaj9","Dbmaj9","Dmaj9","Ebmaj9","Emaj9","Fmaj9","Gbmaj9","Gmaj9","Abmaj9"];
+// const MinorChordsSharpMaj9 = ["Amaj9","A#maj9","Bmaj9","Cmaj9","C#maj9","Dmaj9","D#maj9","Emaj9","Fmaj9","F#maj9","Gmaj9","G#maj9"];
+// const MinorChordsFlatMaj9  = ["Amaj9","Bbmaj9","Bmaj9","Cmaj9","Dbmaj9","Dmaj9","Ebmaj9","Emaj9","Fmaj9","Gbmaj9","Gmaj9","Abmaj9"];
+// const MajorChordsSharpMaj11 = ["Amaj11","A#maj11","Bmaj11","Cmaj11","C#maj11","Dmaj11","D#maj11","Emaj11","Fmaj11","F#maj11","Gmaj11","G#maj11"];
+// const MajorChordsFlatMaj11  = ["Amaj11","Bbmaj11","Bmaj11","Cmaj11","Dbmaj11","Dmaj11","Ebmaj11","Emaj11","Fmaj11","Gbmaj11","Gmaj11","Abmaj11"];
+// const MinorChordsSharpMaj11 = ["Amaj11","A#maj11","Bmaj11","Cmaj11","C#maj11","Dmaj11","D#maj11","Emaj11","Fmaj11","F#maj11","Gmaj11","G#maj11"];
+// const MinorChordsFlatMaj11  = ["Amaj11","Bbmaj11","Bmaj11","Cmaj11","Dbmaj11","Dmaj11","Ebmaj11","Emaj11","Fmaj11","Gbmaj11","Gmaj11","Abmaj11"];
+// const MajorChordsSharpMaj13 = ["Amaj13","A#maj13","Bmaj13","Cmaj13","C#maj13","Dmaj13","D#maj13","Emaj13","Fmaj13","F#maj13","Gmaj13","G#maj13"];
+// const MajorChordsFlatMaj13  = ["Amaj13","Bbmaj13","Bmaj13","Cmaj13","Dbmaj13","Dmaj13","Ebmaj13","Emaj13","Fmaj13","Gbmaj13","Gmaj13","Abmaj13"];
+// const MinorChordsSharpMaj13 = ["Amaj13","A#maj13","Bmaj13","Cmaj13","C#maj13","Dmaj13","D#maj13","Emaj13","Fmaj13","F#maj13","Gmaj13","G#maj13"];
+// const MinorChordsFlatMaj13  = ["Amaj13","Bbmaj13","Bmaj13","Cmaj13","Dbmaj13","Dmaj13","Ebmaj13","Emaj13","Fmaj13","Gbmaj13","Gmaj13","Abmaj13"];
+// const MajorChordsSharpMaj7b5 = ["Amaj7b5","A#maj7b5","Bmaj7b5","Cmaj7b5","C#maj7b5","Dmaj7b5","D#maj7b5","Emaj7b5","Fmaj7b5","F#maj7b5","Gmaj7b5","G#maj7b5"];
+// const MajorChordsFlatMaj7b5  = ["Amaj7b5","Bbmaj7b5","Bmaj7b5","Cmaj7b5","Dbmaj7b5","Dmaj7b5","Ebmaj7b5","Emaj7b5","Fmaj7b5","Gbmaj7b5","Gmaj7b5","Abmaj7b5"];
+// const MinorChordsSharpMaj7b5 = ["Amaj7b5","A#maj7b5","Bmaj7b5","Cmaj7b5","C#maj7b5","Dmaj7b5","D#maj7b5","Emaj7b5","Fmaj7b5","F#maj7b5","Gmaj7b5","G#maj7b5"];
+// const MinorChordsFlatMaj7b5  = ["Amaj7b5","Bbmaj7b5","Bmaj7b5","Cmaj7b5","Dbmaj7b5","Dmaj7b5","Ebmaj7b5","Emaj7b5","Fmaj7b5","Gbmaj7b5","Gmaj7b5","Abmaj7b5"];
+// const MajorChordsSharpMaj7s5 = ["Amaj7#5","A#maj7#5","Bmaj7#5","Cmaj7#5","C#maj7#5","Dmaj7#5","D#maj7#5","Emaj7#5","Fmaj7#5","F#maj7#5","Gmaj7#5","G#maj7#5"];
+// const MajorChordsFlatMaj7s5  = ["Amaj7#5","Bbmaj7#5","Bmaj7#5","Cmaj7#5","Dbmaj7#5","Dmaj7#5","Ebmaj7#5","Emaj7#5","Fmaj7#5","Gbmaj7#5","Gmaj7#5","Abmaj7#5"];
+// const MinorChordsSharpMaj7s5 = ["Amaj7#5","A#maj7#5","Bmaj7#5","Cmaj7#5","C#maj7#5","Dmaj7#5","D#maj7#5","Emaj7#5","Fmaj7#5","F#maj7#5","Gmaj7#5","G#maj7#5"];
+// const MinorChordsFlatMaj7s5  = ["Amaj7#5","Bbmaj7#5","Bmaj7#5","Cmaj7#5","Dbmaj7#5","Dmaj7#5","Ebmaj7#5","Emaj7#5","Fmaj7#5","Gbmaj7#5","Gmaj7#5","Abmaj7#5"];
+// const MajorChordsSharpMaj7b9 = ["Amaj7b9","A#maj7b9","Bmaj7b9","Cmaj7b9","C#maj7b9","Dmaj7b9","D#maj7b9","Emaj7b9","Fmaj7b9","F#maj7b9","Gmaj7b9","G#maj7b9"];
+// const MajorChordsFlatMaj7b9  = ["Amaj7b9","Bbmaj7b9","Bmaj7b9","Cmaj7b9","Dbmaj7b9","Dmaj7b9","Ebmaj7b9","Emaj7b9","Fmaj7b9","Gbmaj7b9","Gmaj7b9","Abmaj7b9"];
+// const MinorChordsSharpMaj7b9 = ["Amaj7b9","A#maj7b9","Bmaj7b9","Cmaj7b9","C#maj7b9","Dmaj7b9","D#maj7b9","Emaj7b9","Fmaj7b9","F#maj7b9","Gmaj7b9","G#maj7b9"];
+// const MinorChordsFlatMaj7b9  = ["Amaj7b9","Bbmaj7b9","Bmaj7b9","Cmaj7b9","Dbmaj7b9","Dmaj7b9","Ebmaj7b9","Emaj7b9","Fmaj7b9","Gbmaj7b9","Gmaj7b9","Abmaj7b9"];
+// const MajorChordsSharpMaj7s9 = ["Amaj7#9","A#maj7#9","Bmaj7#9","Cmaj7#9","C#maj7#9","Dmaj7#9","D#maj7#9","Emaj7#9","Fmaj7#9","F#maj7#9","Gmaj7#9","G#maj7#9"];
+// const MajorChordsFlatMaj7s9  = ["Amaj7#9","Bbmaj7#9","Bmaj7#9","Cmaj7#9","Dbmaj7#9","Dmaj7#9","Ebmaj7#9","Emaj7#9","Fmaj7#9","Gbmaj7#9","Gmaj7#9","Abmaj7#9"];
+// const MinorChordsSharpMaj7s9 = ["Amaj7#9","A#maj7#9","Bmaj7#9","Cmaj7#9","C#maj7#9","Dmaj7#9","D#maj7#9","Emaj7#9","Fmaj7#9","F#maj7#9","Gmaj7#9","G#maj7#9"];
+// const MinorChordsFlatMaj7s9  = ["Amaj7#9","Bbmaj7#9","Bmaj7#9","Cmaj7#9","Dbmaj7#9","Dmaj7#9","Ebmaj7#9","Emaj7#9","Fmaj7#9","Gbmaj7#9","Gmaj7#9","Abmaj7#9"];
+// const MajorChordsSharpMaj7b11 = ["Amaj7b11","A#maj7b11","Bmaj7b11","Cmaj7b11","C#maj7b11","Dmaj7b11","D#maj7b11","Emaj7b11","Fmaj7b11","F#maj7b11","Gmaj7b11","G#maj7b11"];
+// const MajorChordsFlatMaj7b11  = ["Amaj7b11","Bbmaj7b11","Bmaj7b11","Cmaj7b11","Dbmaj7b11","Dmaj7b11","Ebmaj7b11","Emaj7b11","Fmaj7b11","Gbmaj7b11","Gmaj7b11","Abmaj7b11"];
+// const MinorChordsSharpMaj7b11 = ["Amaj7b11","A#maj7b11","Bmaj7b11","Cmaj7b11","C#maj7b11","Dmaj7b11","D#maj7b11","Emaj7b11","Fmaj7b11","F#maj7b11","Gmaj7b11","G#maj7b11"];
+// const MinorChordsFlatMaj7b11  = ["Amaj7b11","Bbmaj7b11","Bmaj7b11","Cmaj7b11","Dbmaj7b11","Dmaj7b11","Ebmaj7b11","Emaj7b11","Fmaj7b11","Gbmaj7b11","Gmaj7b11","Abmaj7b11"];
+// const MajorChordsSharpMaj7s11 = ["Amaj7#11","A#maj7#11","Bmaj7#11","Cmaj7#11","C#maj7#11","Dmaj7#11","D#maj7#11","Emaj7#11","Fmaj7#11","F#maj7#11","Gmaj7#11","G#maj7#11"];
+// const MajorChordsFlatMaj7s11  = ["Amaj7#11","Bbmaj7#11","Bmaj7#11","Cmaj7#11","Dbmaj7#11","Dmaj7#11","Ebmaj7#11","Emaj7#11","Fmaj7#11","Gbmaj7#11","Gmaj7#11","Abmaj7#11"];
+// const MinorChordsSharpMaj7s11 = ["Amaj7#11","A#maj7#11","Bmaj7#11","Cmaj7#11","C#maj7#11","Dmaj7#11","D#maj7#11","Emaj7#11","Fmaj7#11","F#maj7#11","Gmaj7#11","G#maj7#11"];
+// const MinorChordsFlatMaj7s11  = ["Amaj7#11","Bbmaj7#11","Bmaj7#11","Cmaj7#11","Dbmaj7#11","Dmaj7#11","Ebmaj7#11","Emaj7#11","Fmaj7#11","Gbmaj7#11","Gmaj7#11","Abmaj7#11"];
+// const MajorChordsSharpMaj7b13 = ["Amaj7b13","A#maj7b13","Bmaj7b13","Cmaj7b13","C#maj7b13","Dmaj7b13","D#maj7b13","Emaj7b13","Fmaj7b13","F#maj7b13","Gmaj7b13","G#maj7b13"];
+// const MajorChordsFlatMaj7b13  = ["Amaj7b13","Bbmaj7b13","Bmaj7b13","Cmaj7b13","Dbmaj7b13","Dmaj7b13","Ebmaj7b13","Emaj7b13","Fmaj7b13","Gbmaj7b13","Gmaj7b13","Abmaj7b13"];
+// const MinorChordsSharpMaj7b13 = ["Amaj7b13","A#maj7b13","Bmaj7b13","Cmaj7b13","C#maj7b13","Dmaj7b13","D#maj7b13","Emaj7b13","Fmaj7b13","F#maj7b13","Gmaj7b13","G#maj7b13"];
+// const MinorChordsFlatMaj7b13  = ["Amaj7b13","Bbmaj7b13","Bmaj7b13","Cmaj7b13","Dbmaj7b13","Dmaj7b13","Ebmaj7b13","Emaj7b13","Fmaj7b13","Gbmaj7b13","Gmaj7b13","Abmaj7b13"];
+// const MajorChordsSharpMaj7s13 = ["Amaj7#13","A#maj7#13","Bmaj7#13","Cmaj7#13","C#maj7#13","Dmaj7#13","D#maj7#13","Emaj7#13","Fmaj7#13","F#maj7#13","Gmaj7#13","G#maj7#13"];
+// const MajorChordsFlatMaj7s13  = ["Amaj7#13","Bbmaj7#13","Bmaj7#13","Cmaj7#13","Dbmaj7#13","Dmaj7#13","Ebmaj7#13","Emaj7#13","Fmaj7#13","Gbmaj7#13","Gmaj7#13","Abmaj7#13"];
+// const MinorChordsSharpMaj7s13 = ["Amaj7#13","A#maj7#13","Bmaj7#13","Cmaj7#13","C#maj7#13","Dmaj7#13","D#maj7#13","Emaj7#13","Fmaj7#13","F#maj7#13","Gmaj7#13","G#maj7#13"];
+// const MinorChordsFlatMaj7s13  = ["Amaj7#13","Bbmaj7#13","Bmaj7#13","Cmaj7#13","Dbmaj7#13","Dmaj7#13","Ebmaj7#13","Emaj7#13","Fmaj7#13","Gbmaj7#13","Gmaj7#13","Abmaj7#13"];
+// const MajorChordsSharpMaj7b5b9 = ["Amaj7b5b9","A#maj7b5b9","Bmaj7b5b9","Cmaj7b5b9","C#maj7b5b9","Dmaj7b5b9","D#maj7b5b9","Emaj7b5b9","Fmaj7b5b9","F#maj7b5b9","Gmaj7b5b9","G#maj7b5b9"];
+// const MajorChordsFlatMaj7b5b9  = ["Amaj7b5b9","Bbmaj7b5b9","Bmaj7b5b9","Cmaj7b5b9","Dbmaj7b5b9","Dmaj7b5b9","Ebmaj7b5b9","Emaj7b5b9","Fmaj7b5b9","Gbmaj7b5b9","Gmaj7b5b9","Abmaj7b5b9"];
+// const MinorChordsSharpMaj7b5b9 = ["Amaj7b5b9","A#maj7b5b9","Bmaj7b5b9","Cmaj7b5b9","C#maj7b5b9","Dmaj7b5b9","D#maj7b5b9","Emaj7b5b9","Fmaj7b5b9","F#maj7b5b9","Gmaj7b5b9","G#maj7b5b9"];
+// const MinorChordsFlatMaj7b5b9  = ["Amaj7b5b9","Bbmaj7b5b9","Bmaj7b5b9","Cmaj7b5b9","Dbmaj7b5b9","Dmaj7b5b9","Ebmaj7b5b9","Emaj7b5b9","Fmaj7b5b9","Gbmaj7b5b9","Gmaj7b5b9","Abmaj7b5b9"];
+// const MajorChordsSharpMaj7b5s9 = ["Amaj7b5#9","A#maj7b5#9","Bmaj7b5#9","Cmaj7b5#9","C#maj7b5#9","Dmaj7b5#9","D#maj7b5#9","Emaj7b5#9","Fmaj7b5#9","F#maj7b5#9","Gmaj7b5#9","G#maj7b5#9"];
+// const MajorChordsFlatMaj7b5s9  = ["Amaj7b5#9","Bbmaj7b5#9","Bmaj7b5#9","Cmaj7b5#9","Dbmaj7b5#9","Dmaj7b5#9","Ebmaj7b5#9","Emaj7b5#9","Fmaj7b5#9","Gbmaj7b5#9","Gmaj7b5#9","Abmaj7b5#9"];
+// const MinorChordsSharpMaj7b5s9 = ["Amaj7b5#9","A#maj7b5#9","Bmaj7b5#9","Cmaj7b5#9","C#maj7b5#9","Dmaj7b5#9","D#maj7b5#9","Emaj7b5#9","Fmaj7b5#9","F#maj7b5#9","Gmaj7b5#9","G#maj7b5#9"];
+// const MinorChordsFlatMaj7b5s9  = ["Amaj7b5#9","Bbmaj7b5#9","Bmaj7b5#9","Cmaj7b5#9","Dbmaj7b5#9","Dmaj7b5#9","Ebmaj7b5#9","Emaj7b5#9","Fmaj7b5#9","Gbmaj7b5#9","Gmaj7b5#9","Abmaj7b5#9"];
+// const MajorChordsSharpMaj7s5b9 = ["Amaj7#5b9","A#maj7#5b9","Bmaj7#5b9","Cmaj7#5b9","C#maj7#5b9","Dmaj7#5b9","D#maj7#5b9","Emaj7#5b9","Fmaj7#5b9","F#maj7#5b9","Gmaj7#5b9","G#maj7#5b9"];
+// const MajorChordsFlatMaj7s5b9  = ["Amaj7#5b9","Bbmaj7#5b9","Bmaj7#5b9","Cmaj7#5b9","Dbmaj7#5b9","Dmaj7#5b9","Ebmaj7#5b9","Emaj7#5b9","Fmaj7#5b9","Gbmaj7#5b9","Gmaj7#5b9","Abmaj7#5b9"];
+// const MinorChordsSharpMaj7s5b9 = ["Amaj7#5b9","A#maj7#5b9","Bmaj7#5b9","Cmaj7#5b9","C#maj7#5b9","Dmaj7#5b9","D#maj7#5b9","Emaj7#5b9","Fmaj7#5b9","F#maj7#5b9","Gmaj7#5b9","G#maj7#5b9"];
+// const MinorChordsFlatMaj7s5b9  = ["Amaj7#5b9","Bbmaj7#5b9","Bmaj7#5b9","Cmaj7#5b9","Dbmaj7#5b9","Dmaj7#5b9","Ebmaj7#5b9","Emaj7#5b9","Fmaj7#5b9","Gbmaj7#5b9","Gmaj7#5b9","Abmaj7#5b9"];
+// const MajorChordsSharpMaj7s5s9 = ["Amaj7#5#9","A#maj7#5#9","Bmaj7#5#9","Cmaj7#5#9","C#maj7#5#9","Dmaj7#5#9","D#maj7#5#9","Emaj7#5#9","Fmaj7#5#9","F#maj7#5#9","Gmaj7#5#9","G#maj7#5#9"];
+// const MajorChordsFlatMaj7s5s9  = ["Amaj7#5#9","Bbmaj7#5#9","Bmaj7#5#9","Cmaj7#5#9","Dbmaj7#5#9","Dmaj7#5#9","Ebmaj7#5#9","Emaj7#5#9","Fmaj7#5#9","Gbmaj7#5#9","Gmaj7#5#9","Abmaj7#5#9"];
+// const MinorChordsSharpMaj7s5s9 = ["Amaj7#5#9","A#maj7#5#9","Bmaj7#5#9","Cmaj7#5#9","C#maj7#5#9","Dmaj7#5#9","D#maj7#5#9","Emaj7#5#9","Fmaj7#5#9","F#maj7#5#9","Gmaj7#5#9","G#maj7#5#9"];
+// const MinorChordsFlatMaj7s5s9  = ["Amaj7#5#9","Bbmaj7#5#9","Bmaj7#5#9","Cmaj7#5#9","Dbmaj7#5#9","Dmaj7#5#9","Ebmaj7#5#9","Emaj7#5#9","Fmaj7#5#9","Gbmaj7#5#9","Gmaj7#5#9","Abmaj7#5#9"];
+// const MajorChordsSharpMaj7b5b11 = ["Amaj7b5b11","A#maj7b5b11","Bmaj7b5b11","Cmaj7b5b11","C#maj7b5b11","Dmaj7b5b11","D#maj7b5b11","Emaj7b5b11","Fmaj7b5b11","F#maj7b5b11","Gmaj7b5b11","G#maj7b5b11"];
+// const MajorChordsFlatMaj7b5b11  = ["Amaj7b5b11","Bbmaj7b5b11","Bmaj7b5b11","Cmaj7b5b11","Dbmaj7b5b11","Dmaj7b5b11","Ebmaj7b5b11","Emaj7b5b11","Fmaj7b5b11","Gbmaj7b5b11","Gmaj7b5b11","Abmaj7b5b11"];
+// const MinorChordsSharpMaj7b5b11 = ["Amaj7b5b11","A#maj7b5b11","Bmaj7b5b11","Cmaj7b5b11","C#maj7b5b11","Dmaj7b5b11","D#maj7b5b11","Emaj7b5b11","Fmaj7b5b11","F#maj7b5b11","Gmaj7b5b11","G#maj7b5b11"];
+
+
+function transposeChord(chord, amount) {
+  var scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  var normalizeMap = {
+    Cb: "B",
+    Db: "C#",
+    Eb: "D#",
+    Fb: "E",
+    Gb: "F#",
+    Ab: "G#",
+    Bb: "A#",
+    "E#": "F",
+    "B#": "C",
+  };
+  return chord.replace(/[CDEFGAB](b|#)?/g, function (match) {
+    var i =
+      (scale.indexOf(normalizeMap[match] ? normalizeMap[match] : match) +
+        amount) %
+      scale.length;
+    return scale[i < 0 ? i + scale.length : i];
+  });
 }
 
-function transposeChordLine( line, amount ) {
-    amount = amount || 1;
-    let count = 0;
-    let newLine = '';
-
-    while(count < line.length) {
-        if(line[count] >= 'A' && line[count] <= 'G') {
-            let chord = line[count++];
-            while (count < line.length && line[count] !== ' ' && (line[count] < 'A' || line[count] > 'G')) {
-                chord += line[count++];
-            }
-            let newChord = transposeChord(chord, amount);
-            if(newChord.length < chord.length) {    // pad if shorter
-                newChord += " ";
-            }
-            if(newChord.length > chord.length && count < line.length && (line[count] < 'A' || line[count] > 'G')) { // trim if there's space
-                count++;
-            }
-            newLine += newChord;
-        } else {
-            newLine += line[count++];
-        }
-    }
-    return(newLine);
+function transposeChordsInLine(line, steps) {
+  const chordRegex =
+    /(^|\n\s)[A-G][#b]?[m]?(?:maj|min|sus|aug|dim|maj7|min7|5|7|9|11|13)?(?:\/[A-G][#b]?[m]?)?(?=\s\n|$)/g;
+  return line.replace(chordRegex, (match) => {
+    return transposeChord(match, steps);
+  });
 }
 
 var count = 0;
 function updateElement(element, positive) {
   // Get the current value
   let value = element.innerText;
-  value = transposeChordLine(value, positive ? 1 : -1);
+  value = transposeChordsInLine(value, positive ? 1 : -1);
   // Update the element
   element.innerText = value;
 }
+
 function iterateElements(positive) {
-  // Check if the node is an element
-  var spanArray = document.documentElement.getElementsByTagName("span");
-  for (var i = 0; i < spanArray.length; i++) {
-    updateElement(spanArray[i], positive);
+  // Get all elements on the page
+  const allElements = document.querySelectorAll("*");
+
+  // Filter out elements without children
+  const leafElements = [...allElements].filter(
+    (element) => element.childElementCount === 0
+  );
+    console.log(leafElements);
+  // Now 'leafElements' contains all elements without children
+  for (var i = 0; i < leafElements.length; i++) {
+    updateElement(leafElements[i], positive);
   }
+
+  // array = document.getElementsByTagName("td");
+  // for (var i = 0; i < array.length; i++) {
+  //   updateElement(array[i], positive);
+  // }
 }
 
 function increment() {
-    count++;
-    iterateElements(true);
+  count++;
+  iterateElements(true);
 }
 
 function decrement() {
-    count--;
-    iterateElements(false);
+  count--;
+  iterateElements(false);
 }
 
 // Listen for messages from popup.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'increment') {
-        increment();
-    } else if (request.action === 'decrement') {
-        decrement();
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "increment") {
+    increment();
+  } else if (request.action === "decrement") {
+    decrement();
+  }
 });
